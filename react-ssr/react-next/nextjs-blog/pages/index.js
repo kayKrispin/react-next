4c34c30101d71e7a7d1333c0acbc878819470459
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2020-07-09 12:45:49 +0300

    add routing next

--- react-ssr/react-next/nextjs-blog/pages/index.js
+++ react-ssr/react-next/nextjs-blog/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
@@ -13,17 +14,18 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>My name is kripak</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+         Blogs
         </p>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                21 2009
+              </small>
             </li>
           ))}
         </ul>
