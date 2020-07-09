98f02b212d1dc2c7053efb6ea91bcdebb4e0452d
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2020-07-09 13:48:14 +0300

    change api

--- react-ssr/src/api.js
+++ react-ssr/src/api.js
@@ -1,5 +1,5 @@
 export const getFilms = async value => {
-  return await fetch(`http://www.omdbapi.com/?apikey=dfe6d885&s=${value}`)
+  return await fetch(`https://www.omdbapi.com/?apikey=dfe6d885&s=${value}`)
     .then(response => response.json())
     .then(data => {
       if (!data.Search) return [];
